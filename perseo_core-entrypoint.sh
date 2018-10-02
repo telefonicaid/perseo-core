824000a0303ce3bfe5b84c9a78a8f7dc170c47c3
Carlos Blanco <cblanco@ficodes.com>
2018-10-02 18:25:39 +0200

    Restore entrypoint for retrocompatibility

---
+++ perseo_core-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DEFAULT_PERSEO_FE_URL=perseo_fe_endpoint
+
+PERSEO_FE_URL_ARG=${1}
+PERSEO_FE_URL_VALUE=${2}
+if [ "$PERSEO_FE_URL_ARG" == "-perseo_fe_url" ] && [ -z "$PERSEO_FE_URL" ]
+then
+    export PERSEO_FE_URL="$PERSEO_FE_URL_VALUE"
+fi
+
+# We use tomcat from Apache, then will be started using catalina.sh, instead service tomcat
+exec ${CATALINA_HOME}/bin/catalina.sh run && tail -f /var/log/tomcat/catalina.out
