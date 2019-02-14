3e386ac9f0f546eb88509e3ae43d5d2e541f8469
Marisa Reyes Suárez <fiqare-emergya-dev@emergya.com>
2019-02-14 14:59:27 +0100

    Refactor code

--- src/main/java/com/telefonica/iot/perseo/Constants.java
+++ src/main/java/com/telefonica/iot/perseo/Constants.java
@@ -24,9 +24,7 @@ package com.telefonica.iot.perseo;
  * @author brox
  */
 public final class Constants {
-    private Constants() {
-        super();
-    };
+    private Constants() {}
     public static final String SERVICE_FIELD = "service";
     public static final String SUBSERVICE_FIELD = "subservice";
     public static final String CORRELATOR_HEADER = "fiware-correlator";
