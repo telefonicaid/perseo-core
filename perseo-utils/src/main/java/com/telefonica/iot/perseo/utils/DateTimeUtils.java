3bf191edc765dd07c43038549b5c391638a1a849
Ivan Hernandez <ivan.h.cruz@gmail.com>
2019-11-07 21:18:36 +0100

    Fist commit, added perseo-utils module

---
+++ perseo-utils/src/main/java/com/telefonica/iot/perseo/utils/DateTimeUtils.java
@@ -0,0 +1,79 @@
+package com.telefonica.iot.perseo.utils;
+
+import ca.rmen.sunrisesunset.SunriseSunset;
+import org.apache.log4j.pattern.DatePatternConverter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import java.util.Calendar;
+
+public class DateTimeUtils {
+
+    public static Calendar getNextSunise(Calendar day, double latitude, double longitude) {
+
+        Calendar[] sunriseSunset = SunriseSunset.getSunriseSunset(day, latitude, longitude);
+
+        Calendar day2 = (Calendar) day.clone();
+
+        if (sunriseSunset[0].get(Calendar.HOUR_OF_DAY) < Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            day2.add(Calendar.DATE, 1);
+            sunriseSunset = SunriseSunset.getSunriseSunset(day2, latitude, longitude);
+        }
+
+        return sunriseSunset[0];
+    }
+
+    public static Calendar getNextSunset(Calendar day, double latitude, double longitude) {
+
+        Calendar[] sunriseSunset = SunriseSunset.getSunriseSunset(day, latitude, longitude);
+
+        Calendar day2 = (Calendar) day.clone();
+
+        if (sunriseSunset[1].get(Calendar.HOUR_OF_DAY) < Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            day2.add(Calendar.DATE, 1);
+            sunriseSunset = SunriseSunset.getSunriseSunset(day2, latitude, longitude);
+        }
+
+        return sunriseSunset[1];
+    }
+
+    public static long getMilisToNextSunise(Calendar day, double latitude, double longitude) {
+
+        Calendar[] sunriseSunset = SunriseSunset.getSunriseSunset(day, latitude, longitude);
+
+        Calendar day2 = (Calendar) day.clone();
+
+        if (sunriseSunset[0].get(Calendar.HOUR_OF_DAY) < Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            day2.add(Calendar.DATE, 1);
+            sunriseSunset = SunriseSunset.getSunriseSunset(day2, latitude, longitude);
+        }
+
+        return sunriseSunset[0].getTimeInMillis();
+    }
+
+    public static long getMilisToNextSunset(Calendar day, double latitude, double longitude) {
+
+        Calendar[] sunriseSunset = SunriseSunset.getSunriseSunset(day, latitude, longitude);
+
+        Calendar day2 = (Calendar) day.clone();
+
+        if (sunriseSunset[1].get(Calendar.HOUR_OF_DAY) < Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            day2.add(Calendar.DATE, 1);
+            sunriseSunset = SunriseSunset.getSunriseSunset(day2, latitude, longitude);
+        }
+
+        return sunriseSunset[1].getTimeInMillis();
+    }
+
+
+    public static Calendar convertToUTC(String date, String format) {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd z");
+
+
+
+    }
+
+}
