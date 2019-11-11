e91a29ae37054c36e5a2093caee947d3cd39130b
Ivan Hernandez <44349593+IvanHdzC@users.noreply.github.com>
2019-11-11 15:35:34 +0100

    add licence header to tests class

--- perseo-utils/src/test/java/com/telefonica/iot/perseo/utils/DateTimeUtilsTest.java
+++ perseo-utils/src/test/java/com/telefonica/iot/perseo/utils/DateTimeUtilsTest.java
@@ -1,3 +1,23 @@
+ 
+/**
+ * Copyright 2015 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of perseo-core project.
+ *
+ * perseo-core is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2 as published by the Free Software Foundation.
+ *
+ * perseo-core is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with perseo-core. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU General Public License please contact with
+ * iot_support at tid dot es
+ */
+
 package com.telefonica.iot.perseo.utils;
 
 import junit.framework.TestCase;
